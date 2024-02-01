34ffe5fcd5f91361b5ecec59f5c9527811c910c6
Niceiyke <74918186+Niceiyke@users.noreply.github.com>
2024-02-01 17:23:09 +0100

    update

--- backend/expense_tracker/management/models.py
+++ backend/expense_tracker/management/models.py
@@ -4,11 +4,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+Category_Type=(
+    ('income','Income'),("expenses","Expenses")
+)
 
 class Category(models.Model):
     id =models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, unique=True)
-
+    type =models.CharField(max_length=10,choices=Category_Type,default='expenses')
     def __str__(self):
         return self.name
